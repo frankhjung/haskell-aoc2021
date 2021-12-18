e20213a1d9e7cd2dda1aacf2e37ed563b186f2ff
Frank Jung <frankhjung@linux.com>
2021-12-18 11:35:45 +1100

    day 12 - part 2

--- day12-2.hs
+++ day12-2.hs
@@ -35,17 +35,17 @@ Paths (10):
 
 == Answers
 
-cat day12-1.test | ./day12-1
-10
+cat day12-1.test | ./day12-2
+36
 
-cat day12-2.test | ./day12-1
-19
+cat day12-2.test | ./day12-2
+103
 
-cat day12-3.test | ./day12-1
-226
+cat day12-3.test | ./day12-2
+3509
 
-cat day12.data | ./day12-1
-4378
+cat day12.data | ./day12-2
+133621
 
 -}
 import           Control.Monad   (guard)
@@ -61,6 +61,8 @@ newtype Cave = Cave String deriving stock (Show, Eq, Ord)
 data Size = Big | Small deriving stock (Show, Eq)
 -- Paths are the relationships between caves.
 data Path = Path Cave Cave deriving Show
+-- Can revist one small cave excluding start and end.
+data Revisit = REVISIT | NOREVISIT deriving stock (Show, Eq)
 
 start, end :: Cave
 start = Cave "start"
@@ -77,26 +79,27 @@ nextPath c (Path a b)
   | otherwise = Nothing
 
 -- Get all routes going from start to end.
-allRoutes :: [Path] -> [[Cave]]
-allRoutes paths = filter ((==end) . last) $ (start:) <$> go S.empty start
+allRoutes :: Revisit -> [Path] -> [[Cave]]
+allRoutes revist paths = filter ((==end) . last) $ (start:) <$> go S.empty start revist
   where
     -- Find all valid paths.
     --    visited   current   found paths
-    go :: Set Cave -> Cave -> [[Cave]]
-    go visited from = pure [] <> do
+    go :: Set Cave -> Cave -> Revisit -> [[Cave]]
+    go visited from revisit = pure [] <> do
       guard (from /= end)                       -- stop if we're at the end
       p <- paths                                -- read each path
       to <- maybeToList $ nextPath from p       -- get the next cave to visit
       guard (to /= start)                       -- don't go back to start
-      let valid = case size to of
-            Big   -> size from == Small         -- don't cycle from Big to Big
-            Small -> not $ S.member to visited  -- don't revisit a Small
+      let (valid, revisit') = case (size to, S.member to visited, revisit) of
+            (Big, _, rv)       -> (size from == Small, rv) -- don't cycle from Big to Big
+            (Small, False, rv) -> (True, rv)    -- not yet visited
+            (Small, True, rv)  -> (rv == REVISIT, NOREVISIT) -- may be able to revisit small caves
       guard valid                               -- stop if no more valid paths
-      (to :) <$> go (S.insert to visited) to    -- continue with the next cave
+      (to :) <$> go (S.insert to visited) to revisit' -- continue with the next cave
 
 -- sum the display digits from the information in the mangled segments
 solve :: [Path] -> Int
-solve = length . allRoutes
+solve = length . allRoutes REVISIT
 
 -- parse input
 parse :: String -> [Path]
