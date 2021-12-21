a2b0a45d60e4ea85c85cdc3cc6b490f436d8ac7b
Frank Jung <frankhjung@linux.com>
2021-12-21 14:30:46 +1100

    day 14 - part 1 - amended

--- day14-1.hs
+++ day14-1.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 {-
 == Day 14: Part 1 - Extended Polymerization
 
@@ -10,18 +11,14 @@ $ cat day14.data | ./day14-1
 2435
 
 -}
-import qualified Data.Foldable   as S
-import           Data.List       (group, sort)
 import           Data.List.Split (splitOn)
 import qualified Data.Map.Strict as M
-import           Data.Maybe      (fromJust)
 
-type Template = String
 type Element = Char
 type Polymer = [Element]
-type Input = (Template, Rules)
+type Input = (Polymer, Rules)
 type Rule = (Polymer, Element)
-type Rules = M.Map String Element
+type Rules = M.Map Polymer Element
 
 -- Parse the input:
 --  1. part one is list of comma-separated numbers
@@ -41,30 +38,30 @@ parseRule str = (polymer, head element)
     [polymer, element] = splitOn " -> " str
 
 -- intercalate elements to polymer
---      rules    template   working    polymer
-step :: Rules -> Polymer -> Polymer -> Polymer
-step rs (p:ps) [] = step rs ps [p]
-step _  []     ts = ts
-step rs (p:ps) ts = step rs ps (ts <> [e,p])
-  where
-    e = if M.member [last ts,p] rs then fromJust $ M.lookup [last ts,p] rs else p
+--      rules    template   polymer
+applyRules :: Rules -> Polymer -> Polymer
+applyRules _ [] = []
+applyRules _ [a] = [a]
+applyRules rules (a:b:xs) =
+  case M.lookup [a,b] rules of
+    Just c  -> a : c : applyRules rules (b:xs)
+    Nothing -> error "no rule for " ++ [a,b]
 
 solve :: Input -> Int
 solve (template, rules) = result (runApply 10 template)
   where
     -- apply rules to polymer n times
-    runApply :: Int -> Template -> Template
+    runApply :: Int -> Polymer -> Polymer
     runApply 0 polymer = polymer
-    runApply n polymer = runApply (n-1) polymer'
-      where
-        polymer' = S.toList $ step rules polymer []
-    -- calculate result
-    result :: Template -> Int
-    result polymer = most - least
-      where
-        polymerLengths = (map length . group . sort) polymer
-        most = maximum polymerLengths
-        least = minimum polymerLengths
+    runApply n polymer = runApply (n-1) (applyRules rules polymer)
+    --returns difference of maximum vs minimum occurence of an element in polymer
+    result :: Polymer -> Int
+    result polymer = maximum counts - minimum counts
+      where counts = count polymer
+
+-- count the occurences of characters in a string
+count :: String -> M.Map Char Int
+count = M.fromListWith (+) . map (, 1)
 
 -- Read from STDIN to solve the puzzle.
 main :: IO ()
